f7ffdf55c88902cafd4b0c65cdd48c1a1e3d5bd9
kevwu27 <115754187+kevwu27@users.noreply.github.com>
2023-02-16 11:15:24 -0800

    update

--- grade.sh
+++ grade.sh
@@ -1,5 +1,39 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+if [[ -f ListExamples.java ]]
+then
+    echo 'File found'
+else 
+    echo 'Wrong file submitted'
+    exit
+fi 
+
+cp ../TestListExamples.java TestListExamples.java
+
+javac -cp $CPATH *.java 2>compile-error.txt
+if [[ $? -ne 0 ]]
+then
+    cat compile-error.txt
+    echo  'File unable to compile'
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples >output.txt 2>&1
+
+grep -q "OK" output.txt
+if [[ $? -eq 0 ]]
+then
+    echo All test passed!
+    echo You"'"ve got full score!
+    exit
+fi
+
+grep -o -e "Failures: [0-9]\+" output.txt | grep -o -e "[0-9]\+" 2> numFailure
+
+
+exit
